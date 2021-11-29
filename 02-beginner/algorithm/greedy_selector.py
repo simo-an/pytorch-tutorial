5399fc3eccd81b9f110b07f6b21216c85c54a2f1
simumn <seimor@126.com>
2021-11-29 19:36:42 +0800

    Feat: add numpy basic

---
+++ 02-beginner/algorithm/greedy_selector.py
@@ -0,0 +1,10 @@
+'''
+Input
+    idx:   1 2 3 4 5 6 7  8  9  10 11
+    start: 1 3 0 5 3 5 6  8  8  2  12
+    end:   4 5 6 7 8 9 10 11 12 13 14
+Using greedy algorithm to slove this problem
+'''
+
+
+
