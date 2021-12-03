982d192052184e7ef270b0d183b8678a5356d005
simumn <seimor@126.com>
2021-12-03 11:30:32 +0800

    Feat: add resnet

--- 03-intermediate/deep_residual_network.py
+++ 03-intermediate/deep_residual_network.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
+from torch import Tensor
 '''
 残差网络
 paper: https://arxiv.org/pdf/1512.03385.pdf
@@ -51,71 +52,136 @@ test_loader = torch.utils.data.DataLoader(
     shuffle=False
 )
 
-# 3x3 convolution
-def conv3m3(in_channels, out_channels, stride=1, padding=1):
-    return nn.Conv2d(
-        in_channels,
-        out_channels, 
-        kernel_size=3, 
-        stride=stride, 
-        padding=padding
-    )
+expansion: int = 4
 
-# Residual block
-class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = conv3m3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3m3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels: int, base_channels: int, stride=1) -> None:
+        super(Bottleneck, self).__init__()
+        out_channels = base_channels * expansion
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, base_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(base_channels),
+            nn.ReLU(),
+            nn.Conv2d(base_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.relu = nn.ReLU()
+        self.downsample = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1,  bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, input: Tensor):
+        output = self.bottleneck(input)
+        raw = self.downsample(input)
+
+        result = output + raw
+
+        return self.relu(result)
+
+
+class Resnet50(nn.Module):
+    in_channels: int
+    num_classes: int
+    cur_channels: int
+    def __init__(self, in_channels = 3, num_classes = 1000) -> None:
+        super(Resnet50, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.cur_channels = 64
+        
+        self.block1 = self.make_block(64, 3)  # 64   -> 256
+        self.block2 = self.make_block(128, 4) # 256  -> 512
+        self.block3 = self.make_block(256, 6) # 512  -> 1024
+        self.block4 = self.make_block(512, 3) # 1024 -> 2048
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.fc = nn.Linear(2048, num_classes)
     
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(x)       # 通道、宽高不变
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
+    def forward(self, input: Tensor):
+        input = nn.ReLU()(self.conv1(input))
+        input = self.maxpool(input)
 
-        return out
+        input = self.block1(input)
+        input = self.block2(input)
+        input = self.block3(input)
+        input = self.block4(input)
 
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=10):
-        super().__init__()
-        self.in_channels = 16
-        self.conv = conv3m3(3, 16)
-        self.bn = nn.BatchNorm2d(16)
-        self.relu = nn.ReLU(True)
-        self.layer1 = self.make_layer(block, 16, layers[0])
-        self.layer2 = self.make_layer(block, 32, layers[1], 2)
-        self.layer3 = self.make_layer(block, 64, layers[2], 2)
+        input = self.avgpool(input)
+        input = self.fc(torch.flatten(input, 1)) # input.squeeze()
 
-        self.avg_pool = nn.AvgPool2d(kernel_size=8)
-        self.fc = nn.Linear(64, num_classes)
+        return input
 
-    def make_layer(self, block, out_channels, blocks, stride=1):
-        downsample = None
-        if (stride != 1) or (self.in_channels != out_channels):
-            downsample = nn.Sequential(
-                conv3m3(self.in_channels, out_channels, stride=stride),
-                nn.BatchNorm2d(out_channels)
-            )
-        layers = []
-        layers.append(block(self.in_channels, out_channels, stride, downsample))
-        self.in_channels = out_channels
+    def make_block(self, in_channels, bottleneck_num, stride=1) -> nn.Sequential:
+        bottlenecks = []
+        # 将当前 channels 调整到 in_channels
+        bottlenecks.append(Bottleneck(self.cur_channels, in_channels, stride))
+        self.cur_channels = in_channels * expansion # 经过一个Bottleneck则channels变为原来expansion倍
+
+        for _ in range(1, bottleneck_num):
+            bottlenecks.append(Bottleneck(self.cur_channels, in_channels, stride))
+
+        return nn.Sequential(*bottlenecks)
+
+
+model = Resnet50(in_channels=3, num_classes=10).to(device)
+
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# For updating learning rate
+def update_lr(optimizer, lr):    
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+# Train the model
+total_step = len(train_loader)
+curr_lr = learning_rate
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
         
-        for idx in range(1, blocks):
-            layers.append(block(out_channels, out_channels))
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
         
-        return nn.Sequential(*layers)
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}"
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
-    def forward(self, X):
-        pass
+    # Decay learning rate
+    if (epoch+1) % 20 == 0:
+        curr_lr /= 3
+        update_lr(optimizer, curr_lr)
 
+# Test the model
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the model on the test images: {} %'.format(100 * correct / total))
+
+# Save the model checkpoint
+# torch.save(model.state_dict(), 'resnet.ckpt')
